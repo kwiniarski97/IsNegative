5111dfafd1edd51358d2bd5c85ddf161d6ab1833
kwiniarski97 <kwiniarski97@gmail.com>
2018-04-27 17:26:22 +0200

    fixed namespace
    1.02

--- IsNegative/IsNegative.cs
+++ IsNegative/IsNegative.cs
@@ -1,20 +1,17 @@
-﻿namespace IsNegative
+﻿public class IsNegative
 {
-    public class IsNegative
+    public static bool Check(double number)
     {
-        public static bool Check(double number)
-        {
-            return number < 0;
-        }
+        return number < 0;
+    }
 
-        public static bool Check(decimal number)
-        {
-            return number < 0;
-        }
+    public static bool Check(decimal number)
+    {
+        return number < 0;
+    }
 
-        public static bool Check(long number)
-        {
-            return number < 0;
-        }
+    public static bool Check(long number)
+    {
+        return number < 0;
     }
 }
